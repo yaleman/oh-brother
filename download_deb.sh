0e2c4d14648b05bc7e5749dc2e7378ee3a76e3b7
James Hodgkinson <james@terminaloutcomes.com>
2023-03-28 09:06:40 +1000

    adding a comment to the download script

--- download_deb.sh
+++ download_deb.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Downloads the latest version of the Brother scan software.
+
 DOWNLOAD_URL="$(curl -s "https://support.brother.com/g/b/downloadhowto.aspx?c=us_ot&lang=en&prod=ads2400n_all&os=128&dlid=dlf104033_000&flang=4&type3=566" | grep '\.deb' | cut -d'=' -f3 | cut -d'"' -f2)"
 
 if [ -z "${DOWNLOAD_URL}" ]; then
